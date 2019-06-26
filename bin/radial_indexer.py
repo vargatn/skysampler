9e6ccde982d33a715347bd7462f23320b8c7c9cb
Tamas Norbert Varga <vargatn@gmail.com>
2019-06-26 14:01:49 +0200

    radial_indexer demo

--- bin/radial_indexer.py
+++ bin/radial_indexer.py
@@ -1,5 +1,6 @@
 import numpy as np
 import skysampler.indexer as indexer
+import skysampler.paths as paths
 import glob
 
 import argparse
@@ -9,23 +10,21 @@ parser.add_argument("--ibin", type=int, default=-1)
 parser.add_argument('--noclust', action="store_true")
 parser.add_argument('--norands', action="store_true")
 
-zbins = ((0.2, 0.35), (0.35, 0.5), (0.5, 0.65))
-lbins = ((30, 45), (45, 60))
-survey_fnames = np.sort(glob.glob("/e/eser2/vargatn/DES/Y3_DATA/DES_Y3_GOLD_MOF_base*h5"))
-clust_path = "/e/eser2/vargatn/y3_gold_2.2.1_wide_sofcol_run2_redmapper_v6.4.22+2_lgt20_vl02_catalog.fit"
-rands_path = "/e/eser2/vargatn/y3_gold_2.2.1_wide_sofcol_run2_redmapper_v6.4.22+2_randcat_z0.10-0.95_lgt020_vl02.fit"
-fname_root = "/e/eser2/vargatn/DES/Y3_DATA/"
+survey_fnames = np.sort(glob.glob(paths.config["catalogs"]["survey"]["wide_data_expr"]))
+clust_path = paths.config["catalogs"]["targets"]["clust"]
+rands_path = paths.config["catalogs"]["targets"]["rands"]
 
-clust_tag = "multi-indexer-gamma_v001_clust_"
-rands_tag = "multi-indexer-gamma_v001_rands_"
+clust_tag = paths.config["tag"] + "_clust_"
+rands_tag = paths.config["tag"] + "_rands_"
+fname_root = paths.config["work_dir"]
 
 if __name__ == "__main__":
     args = parser.parse_args()
     if not args.noclust:
         print("starting clusters")
         i = 0
-        for z, zbin in enumerate(zbins):
-            for l, lbin in enumerate(lbins):
+        for z, zbin in enumerate(paths.config["parameter_bins"]["redshift_bins"]):
+            for l, lbin in enumerate(paths.config["parameter_bins"]["lambda_bins"]):
                 if args.ibin == -1 or args.ibin == i:
                     print("z", z, "lambda", l)
                     fname_root = fname_root + clust_tag + "_z" + str(z) + "_l" + str(l)
@@ -37,15 +36,15 @@ if __name__ == "__main__":
 
                     survey = indexer.SurveyData(survey_fnames)
                     imaker = indexer.MultiIndexer(survey, target, fname_root)
-                    imaker.run(nprocess=20)
+                    imaker.run(nprocess=paths.config["nproc"])
 
                 i += 1
 
     if not args.noclust:
         print("starting randoms")
         i = 0
-        for z, zbin in enumerate(zbins):
-            for l, lbin in enumerate(lbins):
+        for z, zbin in enumerate(paths.config["parameter_bins"]["redshift_bins"]):
+            for l, lbin in enumerate(paths.config["parameter_bins"]["lambda_bins"]):
                 if args.ibin == -1 or args.ibin == i:
                     print("z", z, "lambda", l)
                     fname_root = fname_root + rands_tag + "_z" + str(z) + "_l" + str(l)
@@ -58,5 +57,5 @@ if __name__ == "__main__":
 
                     survey = indexer.SurveyData(survey_fnames)
                     imaker = indexer.MultiIndexer(survey, random, fname_root)
-                    imaker.run(nprocess=20)
+                    imaker.run(nprocess=paths.config["nproc"])
                 i += 1
